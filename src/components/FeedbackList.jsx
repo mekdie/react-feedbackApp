4c0e0f43dec71f16f0daf1d5394e631f21fa3429
McDony Lee <mcdonyleee@gmail.com>
2022-12-14 02:16:55 +1100

    Added spinner / loading when fetching the task

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,15 +3,17 @@ import FeedbackItem from "./FeedbackItem";
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 const FeedbackList = () => {
     //taking feedback from feedbackContext using useContext hook
-    const { feedback } = useContext(FeedbackContext);
-    if (!feedback || feedback.length === 0) {
+    const { feedback, isLoading } = useContext(FeedbackContext);
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback yet</p>;
     }
-
-    return (
+    return isLoading ? (
+        <Spinner />
+    ) : (
         <div className="feedback-list">
             {/* <AnimatePresence> */}
             {feedback.map((item) => (
