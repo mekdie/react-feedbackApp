e27fea3e25aaf1b6ce7667fd6834905896bc64b5
McDony Lee <mcdonyleee@gmail.com>
2022-12-13 20:12:56 +1100

    Added animation on add and delete task

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 
 const FeedbackList = ({ feedback, onDelete }) => {
     if (!feedback || feedback.length === 0) {
@@ -8,12 +9,35 @@ const FeedbackList = ({ feedback, onDelete }) => {
 
     return (
         <div className="feedback-list">
-            {/*  on every item in feedback return a feedbackitem */}
+            {/* <AnimatePresence> */}
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} onDelete={onDelete} />
+                <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <FeedbackItem
+                        key={item.id}
+                        item={item}
+                        onDelete={onDelete}
+                    />
+                </motion.div>
             ))}
+            {/* </AnimatePresence> */}
         </div>
     );
+
+    //no animation
+
+    // return (
+    //     <div className="feedback-list">
+    //         {/*  on every item in feedback return a feedbackitem */}
+    //         {feedback.map((item) => (
+    //             <FeedbackItem key={item.id} item={item} onDelete={onDelete} />
+    //         ))}
+    //     </div>
+    // );
 };
 
 export default FeedbackList;
